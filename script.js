c8caba041662ebf1ca4d49c8f146347a902ef3ba
D1385357 <D1385357@o365.fcu.edu.tw>
2026-03-26 05:21:26 +0800

    bar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let time = 30;
-let el = document.getElementById("time");
+let time = 15;
+let total = 15;
 
-setInterval(() => {
+let el = document.getElementById("time");
+let progress = document.getElementById("progress");
+
+let timer = setInterval(() => {
     time--;
+
     el.textContent = time;
+
+    let percent = ((total - time) / total) * 100;
+    progress.style.width = percent + "%";
+
+    if (time <= 0){
+        clearInterval(timer);
+        el.textContent = "0";
+        progress.style.width = "100%";
+    }
+
 }, 1000);
 
